d508ee39e7b42b198a917b1805036f89a0276135
hackjamka <mentrible99@gmail.com>
2022-07-21 19:16:39 +0800

    орлого зарлага буцаад хэвэндээ ордог болгосон

--- app.js
+++ app.js
@@ -5,7 +5,22 @@ var uiControler = (function(){
 
 //finace controler
 var finaceControler = (function(){
-
+    var Orlogo = 0;
+    var zarlaga = 0;
+  return{
+    add_orlogo_zarlaga: function(number, addOrRemove){
+          if(addOrRemove== 'inc'){
+            Orlogo += number;
+            document.querySelector('.budget__income--value').textContent = '+ '+Orlogo;
+          }
+          else {
+            zarlaga += number;
+            document.querySelector('.budget__expenses--value').textContent = '- '+ zarlaga;
+            // хувилах
+            document.querySelector('.budget__expenses--percentage').textContent = (100/Orlogo)*zarlaga + '%'
+          }
+    }
+  }
 })();
 
 
@@ -13,17 +28,23 @@ var finaceControler = (function(){
 var appControler=(function(uiControler, finaceControler){
     var add__btn = function(){
         // Өгөгдлөө олж авна.
-        var text = document.querySelector('.add__description').value;
-        var number = document.querySelector('.add__value').value;
-        var addOrRemove =document.querySelector('.add__type').value;
-        console.log(addOrRemove);
+        var textO = document.querySelector('.add__description');
+        var text = textO.value
+        var numberO = document.querySelector('.add__value');
+        var number = numberO.value;
+        var addOrRemoveO =document.querySelector('.add__type');
+        var addOrRemove = addOrRemoveO.value;
         // олсон өгөгдлөө finace controler луу дамжуулна.
-
+        number=Number(number);
         // Нийт өгөгдлөө display гарган.
-
+        finaceControler.add_orlogo_zarlaga(number, addOrRemove);
         // Төсвийг тооцолно.
 
         // Эцсийн өгөгдлөө display гарган.
+        //Буцаагаад хэвэндэн оруулна
+        textO.value = 'Цалин';
+        numberO.value ='0';
+        addOrRemoveO.value = 'inc';
     }
     document.querySelector('.add__btn').addEventListener('click', function(){
         add__btn();
